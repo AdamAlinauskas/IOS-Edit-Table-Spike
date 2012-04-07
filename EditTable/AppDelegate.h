c327141001c989e76952da50b64a235a6b80f420
Adam Alinauskas <aalinauskas@gmail.com>
2012-04-07 16:08:26 -0600

    Initial Commit

---
+++ EditTable/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  EditTable
+//
+//  Created by Adam Alinauskas on 12-04-07.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ EditTable/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  EditTable
+//
+//  Created by Adam Alinauskas on 12-04-07.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
